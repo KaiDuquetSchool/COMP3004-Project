43a95f6acd304c2e36a5d4fb20778924c5729092
Mohammadraza23 <71190315+Mohammadraza23@users.noreply.github.com>
2023-12-09 19:03:50 -0500

    Add files via upload

---
+++ ShockDelivery.h
@@ -0,0 +1,31 @@
+#ifndef SHOCKDELIVERY_H
+#define SHOCKDELIVERY_H
+
+#include <QObject>
+
+class ShockDelivery : public QObject {
+    Q_OBJECT
+
+public:
+    //constructor 
+    ShockDelivery(QObject *parent = nullptr);
+
+    void promptPlaceElectrodes();               // prompting user to place electrodes
+    void promptDeliverShock();                  // prompting  user to deliver a shock if shockable
+    void deliverShock();                        // delivering a shock 
+
+signals:
+    void electrodesPlacementRequested();       // signal for electrode placement
+    void shockDeliveryRequested();             // signal for requesting shock delivery
+    void shockDelivered();                     // signal for when a shock has been delivered
+    void displayMessage(const QString &message); // signal to display message 
+
+private:
+    void simulateShock();                      // simulating the shock delivery to the patient
+    void updatePatientResponse();              // updating the system with the response to the shock
+
+    bool safetyCheckComplete; //checking if safety check has been successfully performed (should be set  to true if so)
+    bool shockDeliveredCheck; ///checking if the shock has successfully been delievered (should be set  to true if so)
+};
+
+#endif 
