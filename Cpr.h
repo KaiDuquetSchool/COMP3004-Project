6ebe94e8e0ab2548d9fcf0debed742a77976ab2c
Mohammadraza23 <71190315+Mohammadraza23@users.noreply.github.com>
2023-12-09 19:19:40 -0500

    Add files via upload

---
+++ Cpr.h
@@ -0,0 +1,31 @@
+#ifndef CPR_H
+#define CPR_H
+
+#include <QObject>
+
+class Cpr : public QObject {
+    Q_OBJECT
+
+public:
+    //constructor
+   Cpr(QObject *parent = nullptr);
+
+    void activateCprPrompt();            // activating prompt to start CPR
+    void start();                        // starting cpr
+    void cprConfirmed();                 // confirming cpr
+    void cprFeedback();                  // feedback for cpr
+
+signals:
+    void cprActivated();                 // signal for cpr acitvation
+    void cprStarted();                   // signal for cpr start
+    void feedbackProvided(const QString &feedback);// signal for feedback
+    void messageDisplayed(const QString &message); // signal for displaying message on display
+
+private:
+    int compressionRate;                 // rate of compressions
+    int compressionDepth;                // depth of compressions
+    void updatePatientResponse();        // updating patients response after Cpr
+
+};
+
+#endif 
